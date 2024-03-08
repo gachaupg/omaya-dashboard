44cc4c398bd0d1266f34c0ab082686b1d1e0a3da
gachaupg <gachaupg607@gmail.com>
2024-03-08 21:46:58 +0300

    ff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
   return (
     <div className='mt-1 w-full flex flex-col items-'>
       <h1>Dashboard</h1>
-      <div className="flex items-center gap-7 flex-wrap">
-        <div style={{width:'22%'}} className="flex flex-col border p-2 rounded-lg shadow-2xl pr-2">
+      <div className="flex items-center gap-3 flex-wrap">
+        <div style={{width:'24%'}} className="flex flex-col border p-2 rounded-lg shadow-2xl pr-2">
           <h3>Total Money Transfer</h3>
           <LineChart
 
@@ -18,13 +18,13 @@ const Home = () => {
                 data: [2, 5.5, 2, 8.5, 1.5, 5],
               },
             ]}
-            width={350}
+            width={300}
             height={280}
           />
           <h2 className='flex items-center'> $ 10.9k span <span className='text-red-600 flex items-center ml-3'> <MdOutlineArrowDropDown />-4.66%</span></h2>
           <p>last week</p>
         </div>
-        <div style={{width:'22%'}} className="flex flex-col border p-2 rounded-lg shadow-2xl pr-2">
+        <div style={{width:'24%'}} className="flex flex-col border p-2 rounded-lg shadow-2xl pr-2">
           <h3>Total Commission</h3>
           <LineChart
             xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
@@ -33,13 +33,13 @@ const Home = () => {
                 data: [2, 5.5, 2, 8.5, 1.5, 5],
               },
             ]}
-            width={350}
+            width={300}
             height={280}
           />
           <h2 className='flex items-center'> $ 10.9k span <span className='text-red-600 flex items-center ml-3'> <MdOutlineArrowDropDown />-4.66%</span></h2>
           <p>last week</p>
         </div>
-        <div style={{width:'22%'}} className="flex flex-col border p-2 rounded-lg shadow-2xl pr-2">
+        <div style={{width:'24%'}} className="flex flex-col border p-2 rounded-lg shadow-2xl pr-2">
           <h3>Total Withdraw</h3>
           <LineChart
             xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
@@ -48,13 +48,13 @@ const Home = () => {
                 data: [2, 5.5, 2, 8.5, 1.5, 5],
               },
             ]}
-            width={350}
+            width={300}
             height={280}
           />
           <h2 className='flex items-center'> $ 10.9k span <span className='text-red-600 flex items-center ml-3'> <MdOutlineArrowDropDown />-4.66%</span></h2>
           <p>last week</p>
         </div>
-        <div style={{width:'22%'}} className="flex flex-col border p-2 rounded-lg shadow-2xl pr-2">
+        <div style={{width:'24%'}} className="flex flex-col border p-2 rounded-lg shadow-2xl pr-2">
           <h3>Total Deposits</h3>
           <LineChart
             xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
@@ -63,7 +63,7 @@ const Home = () => {
                 data: [2, 5.5, 2, 8.5, 1.5, 5],
               },
             ]}
-            width={350}
+            width={300}
             height={280}
           />
           <h2 className='flex items-center'> $ 10.9k span <span className='text-red-600 flex items-center ml-3'> <MdOutlineArrowDropDown />-4.66%</span></h2>
@@ -71,7 +71,7 @@ const Home = () => {
         </div>
       </div>
       <div className="flex w-full items-center gap-12 flex-wrap mt-5">
-        <div style={{width:'39%'}} className="flex flex-col items- justify-center  border bg-slate-100 shadow-2xl rounded-2xl p-2 h-80">
+        <div style={{width:'43%'}} className="flex flex-col items- justify-center  border bg-slate-100 shadow-2xl rounded-2xl p-2 h-80">
           <DonutChart />
         </div>
         <div style={{width:'24%'}} className="flex flex-col items-center justify- pt-3 w-80  border bg-slate-100 shadow-2xl rounded-2xl 2 h-80">
@@ -81,7 +81,7 @@ const Home = () => {
           <DonutChart1 />
         </div>
       </div>
-      <div className="flex items-center mt-8 gap-14 mb-16 flex-wrap">
+      <div className="flex items-center mt-8 gap-1 mb-16 flex-wrap">
         <div className="flex flex-col items- justify-center w-96 border bg-slate-100 shadow-2xl rounded-2xl p-2 h-80">
           <h3>Commission for the month</h3>
           <LineChart
